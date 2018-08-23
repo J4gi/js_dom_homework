8644a389367ceaa2cdcbb287fe92723390ea4fb6
Krystian Jagiełło <jagiello.krystian@gmail.com>
2018-08-23 20:31:08 +0200

    menu

--- 4_menu/js/app.js
+++ 4_menu/js/app.js
@@ -1,3 +1,19 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('działa');
+
+  const mainlis = document.querySelectorAll('.nav>ul>li');
+
+  for (let mainli of mainlis) {
+    mainli.addEventListener('mouseover', function(){
+      if (mainli.querySelector('ul')) {
+        mainli.querySelector('ul').style.display = 'block';
+      }
+
+    });
+    mainli.addEventListener('mouseout', function(){
+      if (mainli.querySelector('ul')) {
+        mainli.querySelector('ul').style.display = 'none';
+      }
+    });
+  }
+});
