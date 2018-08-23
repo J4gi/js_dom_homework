6ac8fd8eb387e273e57f0a5de8e7d0a3d6ec2af3
Krystian Jagiełło <jagiello.krystian@gmail.com>
2018-08-23 19:13:00 +0200

    galeria

--- 2_gallery/js/app.js
+++ 2_gallery/js/app.js
@@ -1,10 +1,36 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function(){
 
-/*
- <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
- </div>
- */
+//console.log('działa');
+const lis = document.querySelectorAll('li');
+const body = document.querySelector('body');
+// console.log(lis[0]);
+// console.log(body);
+
+lis.forEach(function(li){
+
+  //adding evenListener to every picture
+  li.addEventListener('click', function(){
+    let newDiv = document.createElement('div');
+
+    //adding new Div and elements inside it
+    newDiv.className = 'fullScreen';
+    let newImg = document.createElement('img');
+    newImg.src = this.firstChild.src;
+    let newButton = document.createElement('button');
+    newButton.className = 'close';
+    newButton.innerText = 'Close';
+
+    //appending elements to div and div to site
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(newButton);
+    body.appendChild(newDiv);
+
+    //removing div from the site
+    newButton.addEventListener('click', function(){
+      body.removeChild(newDiv);
+    });
+  }); //lis.eventlistener callback function
+}); //lis.forEach callback function
+
+
+});
