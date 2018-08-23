a63155f1c6f5e8c06e9df8940a57e1f3531d3ee3
Krystian Jagiełło <jagiello.krystian@gmail.com>
2018-08-23 19:46:02 +0200

    gallery with filter

--- 3_gallery_with_filter/js/app.js
+++ 3_gallery_with_filter/js/app.js
@@ -5,19 +5,25 @@ const hideButton = document.querySelector('#hideButton');
 const showButton = document.querySelector('#showButton');
 const tagInput = document.querySelector('#tagInput');
 let inputValue = '';
+
 hideButton.addEventListener('click', function(){
-  //console.log(tagInput.value);
   inputValue = tagInput.value;
   tagInput.value = '';
   for (let image of images ) {
-    console.log(image.dataset.tag);
+    if (image.dataset.tag.includes(inputValue)) {
+      image.classList.add('invisible');
+    }
   }
 
 });
 showButton.addEventListener('click', function(){
   inputValue = tagInput.value;
   tagInput.value = '';
-  console.log(inputValue);
+  for (let image of images ) {
+    if (image.dataset.tag.includes(inputValue)) {
+      image.classList.remove('invisible');
+    }
+  }
 });
 
 
