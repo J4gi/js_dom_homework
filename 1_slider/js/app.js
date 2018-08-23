5c57c3e1be19d171a25ece44b4151eefbe2d5b95
Krystian Jagiełło <jagiello.krystian@gmail.com>
2018-08-23 18:12:36 +0200

    slider

--- 1_slider/js/app.js
+++ 1_slider/js/app.js
@@ -1,3 +1,43 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+//1
+document.addEventListener('DOMContentLoaded', function() {
+//2
+const nextButton = document.querySelector('#nextPicture');
+const prevButton = document.querySelector('#prevPicture');
+const listElements = document.querySelectorAll('li');
+let pictureIndex = 0;
+let liClass=listElements[pictureIndex].className;
+//console.log(listElements[pictureIndex]);
+
+//3
+listElements[pictureIndex].className += ' visible';
+console.log(listElements[pictureIndex]);
+
+//4
+nextButton.addEventListener('click', function() {
+//5
+  liClass=liClass.substring(' visible'.length);
+  listElements[pictureIndex].className=liClass;
+//8
+  if (pictureIndex===listElements.length-1) {
+    pictureIndex=0;
+  }else{
+    pictureIndex++;
+  }
+  listElements[pictureIndex].className += ' visible';
+});
+
+prevButton.addEventListener('click', function() {
+//6
+  liClass=liClass.substring(' visible'.length);
+  listElements[pictureIndex].className=liClass;
+//8
+  if (pictureIndex===0) {
+    pictureIndex=listElements.length-1;
+  }else{
+    pictureIndex--;
+  }
+  listElements[pictureIndex].className += ' visible';
+});
+
+
+});
